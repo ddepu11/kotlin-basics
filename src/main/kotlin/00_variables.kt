6a5ff19330ee1e4a0b01de79d9ac1d1d70fcbc76
Deepanshu Tiwari <ddepu11@gmail.com>
2023-04-14 18:39:19 +0530

    learn: var and val

---
+++ src/main/kotlin/00_variables.kt
@@ -0,0 +1,18 @@
+fun main() {
+//  var's can be reassigned
+    var userName: String = "Mohan Kumar"
+    userName = "Dhanush"
+    userName = "Puneet"
+
+
+//  val's can not be reassigned
+    val email: String = "mohan@gmail.com"
+//    email = "dhanush@gmail.com"
+ 
+
+    var age: Int = 23
+    age = 25
+//    age = "22"
+
+    println("Hello $userName, you email is $email your age is $age")
+}
