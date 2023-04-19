a36c81cafb2c3ce3ce7936c5a53f97e4c5beac22
Deepanshu Tiwari <ddepu11@gmail.com>
2023-04-19 19:30:41 +0530

    multiple class constructor

--- src/main/kotlin/multiple_class_constructor_20/User.kt
+++ src/main/kotlin/multiple_class_constructor_20/User.kt
@@ -1,6 +1,15 @@
 package multiple_class_constructor_20
 
-class User(var firstName: String = "", var lastName: String = "smith") {
+class User(var firstName: String = "", var lastName: String = "smith", var isPlatinum: Boolean) {
+
+
+    constructor(firstName: String, lastName: String) : this(firstName, lastName, false) {
+        println("This is secondary constructor.")
+    }
+
+    constructor(firstName: String) : this(firstName, lastName = "Unknown", isPlatinum = true){
+        println("I am third constructor.")
+    }
 
 
     fun printFullName() {
--- src/main/kotlin/multiple_class_constructor_20/main.kt
+++ src/main/kotlin/multiple_class_constructor_20/main.kt
@@ -2,4 +2,19 @@ package multiple_class_constructor_20
 
 fun main() {
 
+//    This is using primary constructor
+    val user = User("Mohan", "Kumar", isPlatinum = true)
+    user.printFullName()
+
+
+//    This is using secondary constructor
+    val friend = User("Umesh", "Paul")
+    friend.printFullName()
+
+
+    val secondFriend = User("Ramesh")
+    secondFriend.printFullName()
+
+
+
 }
