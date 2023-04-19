5eb181f369c6f652a3420d81697fff6106145d7c
Deepanshu Tiwari <ddepu11@gmail.com>
2023-04-19 16:27:07 +0530

    function arguments

--- src/main/kotlin/13_function-arguments.kt
+++ src/main/kotlin/13_function-arguments.kt
@@ -1,3 +1,20 @@
 fun main() {
+    doSomeWork(23, "Mohan", true)
+    doSomeWork(43, "Ajay", false)
+    doSomeWork(6, "Dhruv", true)
+}
+
+// Parameter something that we can pass to a function
+fun doSomeWork(age: Int, name: String, isHappy: Boolean) {
+    println("Your age is $age and name is $name")
+
+    if (age < 21) {
+        println("Age is not greater then 21")
+    } else {
+
+        println("You are old enough")
+    }
+
+    println("Are you happy? $isHappy")
 
 }
