4e2ac2e0ca4eac1604fba0613c520780ca8e4f1d
Deepanshu Tiwari <ddepu11@gmail.com>
2023-04-20 14:19:15 +0530

    getter and setters

---
+++ src/main/kotlin/getters_setters_23/User.kt
@@ -0,0 +1,21 @@
+package getters_setters_23
+
+class User(var firstName: String, lastName: String) {
+
+
+    //    Here we are overriding the get and set implementation of fullName
+    var fullName: String = "$firstName $lastName"
+        get() = "Full Name: $field"
+        set(value) {
+            if (value === "Mohan") {
+                field = "Mohan Kumar"
+            } else {
+                field = value
+            }
+
+        }
+
+//   field is the backing field for fullName property, field contains the content of
+//   full name property.
+
+}
---
+++ src/main/kotlin/getters_setters_23/main.kt
@@ -0,0 +1,18 @@
+package getters_setters_23
+
+fun main() {
+    val user = User("Shubham", "Singh")
+
+    println(user.fullName)
+
+    user.fullName = "Mohan"
+
+    println(user.fullName)
+
+    user.fullName = "Arjun Ramesh"
+
+    println(user.fullName)
+
+
+
+}
