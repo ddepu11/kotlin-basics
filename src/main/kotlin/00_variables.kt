8218b85cd03340ddab369108c5fc8d3ff4e103f1
deepanshu tiwari <ddepu11@gmail.com>
2023-08-14 17:57:10 +0530

    Add more examples

--- src/main/kotlin/00_variables.kt
+++ src/main/kotlin/00_variables.kt
@@ -2,16 +2,26 @@ fun main() {
 //    var is used to create mutable variable
 //    val is used to create read only variable
 
-//  var's can be reassigned
+//    var's can be reassigned
     var userName: String = "Mohan Kumar"
     userName = "Dhanush"
     userName = "Puneet"
 
+    var address = "HNo 2664, Punjab Bank Colony, Jabalpur"
+    address = "AVS Pg Sudhamnagar, Bengluru"
+
+//    println("The Address: $address")
 
 //  val's can not be reassigned
     val email: String = "mohan@gmail.com"
 //    email = "dhanush@gmail.com"
 //    email = ""
+
+    val fatherName = "Shyaml Lal"
+//    fatherName = "New Name"  // Cant do this coz val is radonly, it can not be reassigned.
+
+    println("Father's Name: $fatherName")
+
 
     println("Is email Empty?  ${email.isEmpty()}")
 
@@ -19,5 +29,9 @@ fun main() {
     age = 25
 //    age = "22"
 
-    println("Hello $userName, you email is $email your age is $age")
+
+    var designation = "SE-I"
+     designation = "SE-II"
+
+    println("Hello $userName, you email is $email your age is $age and designation is $designation")
 }
