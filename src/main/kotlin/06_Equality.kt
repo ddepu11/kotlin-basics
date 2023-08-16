9a5b5ac255589b561e9f03f63ff09a496d7d7207
deepanshu tiwari <ddepu11@gmail.com>
2023-08-16 12:37:17 +0530

    add comment and revision

--- src/main/kotlin/06_Equality.kt
+++ src/main/kotlin/06_Equality.kt
@@ -12,26 +12,43 @@ fun main() {
 //    println(name1 != name2)
 
 
-//    Referential equality
-//    var a = 12
-//    var b = 12
+//    Referential/identity/reference  equality
+    var a = 12
+    var b = 12
 
 //    coz every thing in kotlin is object so below
-//    should be false but it true
+//    should be false, but it is true
 //    Coz numbers, characters and boolean all have
 //    special internal representation that is primitive at run time
-//    but to us they look like ordinary classes, so == is same as ===
-//    when when  using primitives
-//    println(a === b)
+//    but to us, they look like ordinary classes, so == is same as ===
+//    when  using primitives
+//    println("Referential Equality: ${a === b}")
 
 
-    var a = Person("John")
-    var b = Person("John")
+    var person1 = Person("John")
+    var person2 = Person("John")
 
-    println(a === b) // false
+//    println("Is Person1 Equals to Person2? ${person1 === person2}") // false
 //    coz they are not same objects
 
-    println(a !== b)
+    println(person1 != person2)
+
+//    In this code, two Person objects, person1 and person2,
+//    are created with the same name "John". When you use the != operator to compare these objects,
+//    you're testing for structural inequality,
+//    which compares the content of the objects. Since the Person class doesn't override the equals() method,
+//    the default behavior is used,
+//    which is based on referential equality (memory addresses).
+
+//    Since person1 and person2 are two separate objects with different memory addresses
+//    (even though they have the same content), the != operator will return true,
+//    indicating that they are not structurally equal.
+
+
+    println(person1 !== person2)
+
+//    Remember that referential equality (===) is about comparing memory addresses and object identity,
+//    while structural equality (==) is about comparing the content or structure of objects.
 
 }
 
