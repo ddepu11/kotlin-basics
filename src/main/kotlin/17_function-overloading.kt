c82f832342b570c367c70c715d05a00ffe6ca1d0
deepanshu tiwari <ddepu11@gmail.com>
2023-08-17 23:33:09 +0530

    add more example

--- src/main/kotlin/17_function-overloading.kt
+++ src/main/kotlin/17_function-overloading.kt
@@ -1,8 +1,25 @@
+// Function overloading in Kotlin allows you to define multiple functions with the same name
+// but different parameter lists. This enables you to provide different implementations of a
+// function depending on the types or number of arguments passed to it. Kotlin's function overloading is a
+// form of polymorphism that enhances code readability and re-usability.
+
 fun main() {
     printUserInfo2("Agrit")
     printUserInfo2("Mohan", 23)
 
     printUserInfo2("Mohan", 23, "Blue")
+
+    println("\n")
+
+    doSomeExercise("Squates")
+    doSomeExercise("Pull ups",  false)
+
+    println("\n")
+
+    printValue(23)
+
+    printValue("Tere te")
+
 }
 
 //fun printUserInfo2(name: String) {
@@ -25,3 +42,25 @@ fun printUserInfo2(name: String, age: Int = 0, favColor: String = "") {
 
 }
 
+
+
+fun doSomeExercise(name:String){
+    println("Exercise Name $name")
+}
+
+fun doSomeExercise(name:String,isItHard:Boolean){
+    println("Exercise Name $name | Is it hard: $isItHard")
+
+}
+
+
+//Parameter Types for Overloading:
+//Kotlin allows you to differentiate overloaded functions by their parameter types as well:
+
+fun printValue(number:Int){
+    println("Number: $number")
+}
+
+fun printValue(text:String){
+    println("Text: $text")
+}
