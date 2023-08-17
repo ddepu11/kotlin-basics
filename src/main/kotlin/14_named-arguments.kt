86a7344ed327244eaa69cda50a18d49ece6fe7cf
deepanshu tiwari <ddepu11@gmail.com>
2023-08-17 22:18:30 +0530

    add more example

--- src/main/kotlin/14_named-arguments.kt
+++ src/main/kotlin/14_named-arguments.kt
@@ -1,8 +1,25 @@
+// In Kotlin, named arguments allow you to specify
+// function parameters by their names rather than their positions.
+// This can make your code more readable and maintainable,
+// especially when calling functions with multiple parameters or default values.
+
 fun main() {
 //    printUserInfo(firstName = "Mohan", lastName = "Kumar", age = 23, isSunburned = false, likesMovies = true, lovesPopcord = false)
-//    Using name arguments we can also change the position of it.
 
+//    Mixing Positional and Named Arguments:
+//    Using name arguments we can also change the position of it.
+//    You can also mix positional and named arguments.
+    //  However, once you start using named arguments for one parameter,
+    //  all the following arguments must be named:
     printUserInfo(age = 23, likesMovies = true, firstName = "Mohan", lastName = "Kumar",  isSunburned = false,  lovesPopcord = false)
+
+    println("\n")
+
+    logFoodBaughtToday(didYouLike = false, price = 60, location = "Langford Garden, Bengluru", name = "Kulfi icecream")
+
+    println("\n")
+
+    logFoodBaughtToday(didYouLike = true, location = "Richmod Circle, Bengluru", name = "Rava Dosa")
 
 }
 
@@ -15,4 +32,9 @@ fun printUserInfo(
     lovesPopcord: Boolean
 ) {
     println("$firstName $lastName age is $age. Sunburned: $isSunburned, Likes Movies: $likesMovies, Likes Popcorn: $lovesPopcord")
+}
+
+
+fun logFoodBaughtToday (name:String,price:Int=100,didYouLike:Boolean,location:String){
+     println("You baught $name whose prise was $price and you ${if(didYouLike)"liked" else "disliked"} it, you baught it in $location")
 }
--- src/main/kotlin/15_default-arguments.kt
+++ src/main/kotlin/15_default-arguments.kt
@@ -1,3 +1,10 @@
+import java.time.Duration
+
+// In Kotlin, default arguments allow you to specify default values for function parameters.
+// This means that if a value is not provided for a particular parameter when the function is called,
+// the default value will be used. This can be quite handy when you want to make some function parameters
+// optional while providing sensible default values.
+
 fun main() {
 
     printUserInfo1(
@@ -27,6 +34,15 @@ fun main() {
         age = 34
     )
 
+    println("\n")
+
+    logFavoriteSong(singer = "Udit Narayan", name = "Tu Mere Samne", duration = "6 min", released = "1996")
+
+    println("\n")
+
+    logFavoriteSong(singer = "Kumar Sanu", name = "Mai Tughko Bhaga Laya", duration = "5 min", released = "1993", doYouLikeIt = true, isItAvailableOnSpotify = true)
+
+
 }
 
 fun printUserInfo1(
@@ -40,3 +56,10 @@ fun printUserInfo1(
 ) {
     println("$firstName $lastName age is $age. Sunburned: $isSunburned, Likes Movies: $likesMovies, Likes Popcorn: $lovesPopcord, Favorite Color: $favoriteColor")
 }
+
+
+fun logFavoriteSong(name:String,released:String,duration:String,isItAvailableOnSpotify:Boolean=false, doYouLikeIt:Boolean= false, singer:String){
+  println("Song Name: $name, Released On: $released, Duration: $duration, It is ${if(isItAvailableOnSpotify)"Available" else "Not Available"} on Spotify, Do You Like: $doYouLikeIt, Singer Name: $singer")
+}
+
+
