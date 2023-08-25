63cb6c26c0e48bbb7e7ba104651e9e2e32d72e68
deepanshu tiwari <ddepu11@gmail.com>
2023-08-25 16:29:29 +0530

    add more example

--- src/main/kotlin/declare_constants_27/User.kt
+++ src/main/kotlin/declare_constants_27/User.kt
@@ -1,16 +1,16 @@
 package declare_constants_27
 
 class User(var firstName: String, lastName: String) {
-//    Constants
+//  Constants
 
-//    MAX_AGE is scoped to the instance of this class
+//  MAX_AGE is scoped to the instance of this class
     val MAX_AGE = 18;
 
     companion object{
         const val MAX_SCORE:Int = 100
 
-//        This cant be accessed outside of this class
-//       private const val MAX_SCORE:Int = 100
+//  This cant be accessed outside of this class
+//  private const val MAX_SCORE:Int = 100
     }
 
 //    MAX_AGE is private to the scope of the class
--- src/main/kotlin/declare_constants_27/main.kt
+++ src/main/kotlin/declare_constants_27/main.kt
@@ -1,6 +1,26 @@
 package declare_constants_27
 
 
+// In Kotlin, constants are values that are assigned once and cannot be changed afterward.
+// They are often used to hold values that are not supposed to be modified during the execution of a program.
+// Kotlin provides two primary ways to define constants:
+
+// Using the val Keyword: You can define constants using the val keyword.
+// This creates an immutable property that can't be reassigned after its initial value is set.
+
+// Using the const Modifier: If you want to ensure that a constant is actually a compile-time constant (i.e. its value is determined at compile time),
+// you can use the const modifier in combination with the val keyword. However, not all types can be declared as const in Kotlin.
+// The types that can be used with the const modifier are very limited:
+
+// Primitive Types: Only primitive types like Int, Long, Short, Byte, Char, Float, and Double can be declared as const.
+
+// String: String literals can also be declared as const.
+
+// Enum Members: Members of an enum class can be declared as const if the enum itself is marked as const.
+
+// Top-Level or Member Objects: If an object is declared at the top level of a file or as a member of a class and consists of only const properties, it can be treated as a compile-time constant.
+
+
 const val MAX_AGE = 18
 
 fun main() {
@@ -8,13 +28,12 @@ fun main() {
     val user = User("Mohan", "Kumar")
     user.MAX_AGE
 
-//    Can access  MAX_SCORE like this coz inside a
-//    companion object
-//    user.MAX_SCORE
+//  Can't access MAX_SCORE like this because it is inside a companion object
+//  user.MAX_SCORE
 
 
 //    But we can access it like
-//    User.MAX_SCORE
+    User.MAX_SCORE
 
 
 
