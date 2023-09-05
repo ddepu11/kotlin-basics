531527146a14abfa91ce486b33fd84ef1445fd20
deepanshu tiwari <ddepu11@gmail.com>
2023-09-05 17:31:42 +0530

    example of iterate over enums

---
+++ src/main/kotlin/iterate_over_enums/main.kt
@@ -0,0 +1,19 @@
+package iterate_over_enums
+
+fun main() {
+//    for(accountType in AccountType.values()){
+//        println("Account Type: $accountType \n")
+//    }
+
+
+//  Same as above
+    AccountType.values().forEach { println("Account Type: $it") }
+}
+
+
+enum class AccountType {
+    BRONZE,
+    PLATINUM,
+    SILVER,
+    GOLD
+}
