2c9c6acd14433b2891eec51f43e8b1eac041496f
deepanshu tiwari <ddepu11@gmail.com>
2023-09-06 15:34:20 +0530

    add exmaples for copy data of class instance

--- src/main/kotlin/Copying_Data_Class_Instances_38/copy_data_class_instance.kt
+++ src/main/kotlin/Copying_Data_Class_Instances_38/copy_data_class_instance.kt
@@ -2,4 +2,32 @@ package Copying_Data_Class_Instances_38
 
 fun main() {
 
+    val person1 = Person("Pawan","Singh",23)
+
+//  Creating the copy of the data
+    val sibling = person1.copy()
+    println(person1)
+    println(sibling)
+
+//  Can also change some props
+    val sibling2 = person1.copy(firstName = "Mrunal", age = 23)
+    println(sibling2)
+
+
+    val order = Order(amount = 24, costumer = sibling2)
+
+    println(order)
+
+//  New amount but with same costumer.
+    val newOrder = order.copy(amount = 200)
+
+    println("New Order $newOrder")
+
+//  Same order but with different costumer
+    val newOrder2 = order.copy(costumer = person1)
+    println("New Order 2 $newOrder2")
 }
+
+data class Order(val amount:Int,val costumer:Person)
+
+data class Person(val firstName:String,val lastName:String, val age:Int)
