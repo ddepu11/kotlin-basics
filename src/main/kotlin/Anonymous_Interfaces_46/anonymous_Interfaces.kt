ba5584e40877904363847081d79b9163b44e0294
deepanshu tiwari <ddepu11@gmail.com>
2023-09-12 13:12:09 +0530

    add examples of anonymous interface

---
+++ src/main/kotlin/Anonymous_Interfaces_46/anonymous_Interfaces.kt
@@ -0,0 +1,70 @@
+package Anonymous_Interfaces_46
+
+fun main(){
+
+    val button = Button()
+
+//  For concrete implementation
+//    button.clickListener = MyListener()
+//    button.click("Button")
+
+
+//    button.clickListener = object :OnClickListener{
+//        override fun onClick(viewName: String) {
+//            println("On Click Clicked. $viewName")
+//        }
+//
+//    }
+
+//    OR
+
+    button.setMyListener(object :OnClickListener{
+        override fun onClick(viewName: String) {
+            println("On Click Clicked. $viewName abc")
+        }
+
+    })
+
+    button.click("Button")
+
+
+//    MAP
+
+    val map = Map()
+    map.clickListener = object :OnClickListener{
+        override fun onClick(viewName: String) {
+            println("$viewName Clicked!!!")
+        }
+
+    }
+    map.click("MAP")
+}
+
+
+// Below is a concrete implementation
+//class MyListener:OnClickListener{
+//    override fun onClick(viewName: String) {
+//        println("$viewName Clicked.")
+//    }
+//
+//}
+
+interface  OnClickListener{
+    fun onClick(viewName:String)
+}
+
+abstract  class View{
+    lateinit var clickListener: OnClickListener
+
+    fun setMyListener(clickListener: OnClickListener ) {
+            this.clickListener = clickListener
+    }
+    fun click( viewName:String){
+        clickListener.onClick(viewName)
+    }
+}
+
+
+class Button:View()
+class Image:View()
+class Map:View()
