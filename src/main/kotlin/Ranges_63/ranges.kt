d3ece26978740eb7c5c2e8d140f44e3d5252a421
deepanshu tiwari <ddepu11@gmail.com>
2023-09-20 17:40:18 +0530

    add ranges examples

---
+++ src/main/kotlin/Ranges_63/ranges.kt
@@ -0,0 +1,17 @@
+package Ranges_63
+
+fun main(){
+    var i = 12
+
+    if(i in 1..5){
+        println("YES")
+    }else{
+        println("NO")
+    }
+
+    println("#$#$#$#$#$#$")
+
+    for (j in 0..8 step 2){
+        println("Inside Loop $j")
+    }
+}
