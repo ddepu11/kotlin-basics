0a84a98347c03e4b8048e3108e4e4b1f33008c42
deepanshu tiwari <ddepu11@gmail.com>
2023-09-27 22:19:19 +0530

    add comment

--- src/main/kotlin/trycatch_vs_trycatchfinally_84/trycatch_finally_84.kt
+++ src/main/kotlin/trycatch_vs_trycatchfinally_84/trycatch_finally_84.kt
@@ -1,15 +1,17 @@
 package trycatch_vs_trycatchfinally_84
 
 import java.lang.Exception
-import java.lang.IllegalArgumentException
 import java.lang.IllegalStateException
 
 fun main(){
     val person = Person("Kirtan",17)
 
+//  Finally will always be called, but it's not guaranteed that the catch will be called as it is based on the
+//  Type of exception
+
     try {
        checkAge(person)
-    }catch (ex:IllegalStateException){
+    }catch (ex:IllegalArgumentException){
         println("Reached catch block!")
     }finally {
         println("Reached Finally!")
