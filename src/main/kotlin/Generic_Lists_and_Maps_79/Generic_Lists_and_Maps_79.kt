1ef881f4f3a25095e51d7098a24e4c82a26c7bfb
deepanshu tiwari <ddepu11@gmail.com>
2023-09-27 15:54:18 +0530

    add example of generic lists and map

---
+++ src/main/kotlin/Generic_Lists_and_Maps_79/Generic_Lists_and_Maps_79.kt
@@ -0,0 +1,15 @@
+package Generic_Lists_and_Maps_79
+
+fun main(){
+//  Generic Lists and Maps
+
+    val listOfStrings:List<String> = listOf("Mohan","Sohan","Rohan","Anubhav")
+    val listOfInts:List<Int> = listOf(12,43,54,90)
+
+    val map:Map<Int,String> = mapOf(1 to "Sun",2 to "Napchune",3 to "Mars", 4 to "Earth",5 to "Mercury")
+
+
+
+
+
+}
