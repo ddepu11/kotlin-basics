e2fb287aeef070c57971a3431f637ecf291a0180
deepanshu tiwari <ddepu11@gmail.com>
2023-09-29 19:08:05 +0530

    fix spelings

--- src/main/kotlin/Extension_Functions_85/Extension_Functions.kt
+++ src/main/kotlin/Extension_Functions_85/Extension_Functions.kt
@@ -3,17 +3,17 @@ package Extension_Functions_85
 fun main(){
     val name:String  = "Mohan Kumar"
 
-//  Initials is extension function
+//  Initials are extension function
     println(name.initials())
 
     val age:Int = 15
 
-// Is adult is extenion function
+// Is adult is extension function
     println("Is Adult: ${age.isAdult()}")
 
     val person = Person("Mukesh","Khanna",34)
 
-//  Full name is extenion function
+//  Full name is extension function
     println(person.fullName())
 }
 
