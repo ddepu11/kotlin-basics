e3aae1a65942247aa90da31550aeb92dbf54b425
deepanshu tiwari <ddepu11@gmail.com>
2023-10-01 21:19:56 +0530

    it paramater

---
+++ src/main/kotlin/it_Paramter_in_Lambda_Functions_95/it_paramter_in_lambda_functions_95.kt
@@ -0,0 +1,7 @@
+package it_Paramter_in_Lambda_Functions_95
+
+fun main() {
+//  When you have only one parameter, then kotlin map it with 'it'.
+    repeat(3) { println("This is iteration ${it + 1}") }
+
+}
